77e15f9f0d1b77b86426e4140b43b7e1316921a5
Eduardo <eduardo20041995@hotmail.com>
2024-09-14 18:11:49 -0400

    Corrige migrações

--- rest/src/conversa/conversa.migracoes.pas
+++ rest/src/conversa/conversa.migracoes.pas
@@ -16,104 +16,99 @@ implementation
 uses
   Data.DB;
 
-const v0 =
-  sl +'create '+
-  sl +' table usuario  '+
-  sl +'     ( id serial4 not null '+
-  sl +'     , nome varchar(100) not null '+
-  sl +'     , login varchar(50) not null '+
-  sl +'     , email varchar(100) not null '+
-  sl +'     , telefone varchar(50) null '+
-  sl +'     , senha varchar(50) not null '+
-  sl +'     , constraint usuario_email_unique unique (email) '+
-  sl +'     , constraint usuario_pk primary key (id) '+
-  sl +'     ); '+
-  sl +
-  sl +'create '+
-  sl +' table conversa  '+
-  sl +'     ( id serial4 not null '+
-  sl +'     , descricao varchar(100) null '+
-  sl +'     , tipo int4 default 1 not null -- 1-chat; 2-grupo '+
-  sl +'     , inserida timestamp default current_timestamp not null '+
-  sl +'     , constraint conversa_pk primary key (id) '+
-  sl +'     ); '+
-  sl +
-  sl +'comment on column public.conversa.tipo is ''1-Chat; 2-Grupo''; '+
-  sl +
-  sl +'create '+
-  sl +' table usuario_contato  '+
-  sl +'     ( id serial4 not null '+
-  sl +'     , usuario_id int4 null '+
-  sl +'     , relacionamento_id int4 null '+
-  sl +'     , constraint usuario_contato_pk primary key (id) '+
-  sl +'     , constraint usuario_contato_usuario_fk foreign key (usuario_id) references usuario(id) '+
-  sl +'     , constraint usuario_contato_usuario_fk_1 foreign key (relacionamento_id) references usuario(id) '+
-  sl +'     ); '+
-  sl +
-  sl +'create '+
-  sl +' table conversa_usuario  '+
-  sl +'     ( id serial4 not null '+
-  sl +'     , usuario_id int4 not null '+
-  sl +'     , conversa_id int4 not null '+
-  sl +'     , constraint conversa_usuario_pk primary key (id) '+
-  sl +'     , constraint conversa_usuario_conversa_fk foreign key (conversa_id) references conversa(id) '+
-  sl +'     , constraint conversa_usuario_usuario_fk foreign key (usuario_id) references usuario(id) '+
-  sl +'     ); '+
-  sl +
-  sl +'create '+
-  sl +' table mensagem  '+
-  sl +'     ( id serial4 not null '+
-  sl +'     , usuario_id int4 not null '+
-  sl +'     , conversa_id int4 not null '+
-  sl +'     , inserida timestamp default current_timestamp not null'+
-  sl +'     , alterada timestamp null '+
-  sl +'     , constraint mensagem_pk primary key (id) '+
-  sl +'     , constraint mensagem_conversa_fk foreign key (conversa_id) references conversa(id) '+
-  sl +'     , constraint mensagem_usuario_fk foreign key (usuario_id) references usuario(id) '+
-  sl +'     ); '+
-  sl +
-  sl +'create '+
-  sl +' table mensagem_conteudo  '+
-  sl +'     ( id serial4 not null '+
-  sl +'     , mensagem_id int4 not null '+
-  sl +'     , ordem int4 not null '+
-  sl +'     , tipo int4 not null '+
-  sl +'     , conteudo bytea null '+
-  sl +'     , constraint mensagem_conteudo_pk primary key (id) '+
-  sl +'     , constraint mensagem_conteudo_mensagem_fk foreign key (mensagem_id) references mensagem(id) '+
-  sl +'     ); '+
-  sl +
-  sl +'create '+
-  sl +' table mensagem_status '+
-  sl +'     ( conversa_id int4 not null '+
-  sl +'     , usuario_id int4 not null '+
-  sl +'     , mensagem_id int4 not null '+
-  sl +'     , recebida timestamp null '+
-  sl +'     , visualizada timestamp null '+
-  sl +'     , reproduzida timestamp null '+
-  sl +'     , constraint mensagem_status_conversa_fk foreign key (conversa_id) references conversa(id) '+
-  sl +'     , constraint mensagem_status_mensagem_fk foreign key (mensagem_id) references mensagem(id) '+
-  sl +'     , constraint mensagem_status_usuario_fk foreign key (usuario_id) references usuario(id) '+
-  sl +'     ); '+
-  sl +
-  sl +'create '+
-  sl +' table anexo '+
-  sl +'     ( id serial4 not null '+
-  sl +'     , identificador varchar(64) not null '+
-  sl +'     , tipo int4 not null '+
-  sl +'     , tamanho int4 not null '+
-  sl +'     , constraint anexo_pk primary key (id) '+
-  sl +'     ); '+
-  sl +
-  sl +'update parametros '+
-  sl +'   set valor = ''0'' '+
-  sl +' where nome  = ''versao'' ';
-
 const
-  v0_1 =
-  sl +'ALTER TABLE public.anexo ADD nome varchar(255) NULL;'+
-  sl +'ALTER TABLE public.anexo ADD extensao varchar(10) NULL;';
+  Versoes: Array[0..1] of String = (
+    sl +'create '+
+    sl +' table usuario  '+
+    sl +'     ( id serial4 not null '+
+    sl +'     , nome varchar(100) not null '+
+    sl +'     , login varchar(50) not null '+
+    sl +'     , email varchar(100) not null '+
+    sl +'     , telefone varchar(50) null '+
+    sl +'     , senha varchar(50) not null '+
+    sl +'     , constraint usuario_email_unique unique (email) '+
+    sl +'     , constraint usuario_pk primary key (id) '+
+    sl +'     ); '+
+    sl +
+    sl +'create '+
+    sl +' table conversa  '+
+    sl +'     ( id serial4 not null '+
+    sl +'     , descricao varchar(100) null '+
+    sl +'     , tipo int4 default 1 not null -- 1-chat; 2-grupo '+
+    sl +'     , inserida timestamp default current_timestamp not null '+
+    sl +'     , constraint conversa_pk primary key (id) '+
+    sl +'     ); '+
+    sl +
+    sl +'comment on column public.conversa.tipo is ''1-Chat; 2-Grupo''; '+
+    sl +
+    sl +'create '+
+    sl +' table usuario_contato  '+
+    sl +'     ( id serial4 not null '+
+    sl +'     , usuario_id int4 null '+
+    sl +'     , relacionamento_id int4 null '+
+    sl +'     , constraint usuario_contato_pk primary key (id) '+
+    sl +'     , constraint usuario_contato_usuario_fk foreign key (usuario_id) references usuario(id) '+
+    sl +'     , constraint usuario_contato_usuario_fk_1 foreign key (relacionamento_id) references usuario(id) '+
+    sl +'     ); '+
+    sl +
+    sl +'create '+
+    sl +' table conversa_usuario  '+
+    sl +'     ( id serial4 not null '+
+    sl +'     , usuario_id int4 not null '+
+    sl +'     , conversa_id int4 not null '+
+    sl +'     , constraint conversa_usuario_pk primary key (id) '+
+    sl +'     , constraint conversa_usuario_conversa_fk foreign key (conversa_id) references conversa(id) '+
+    sl +'     , constraint conversa_usuario_usuario_fk foreign key (usuario_id) references usuario(id) '+
+    sl +'     ); '+
+    sl +
+    sl +'create '+
+    sl +' table mensagem  '+
+    sl +'     ( id serial4 not null '+
+    sl +'     , usuario_id int4 not null '+
+    sl +'     , conversa_id int4 not null '+
+    sl +'     , inserida timestamp default current_timestamp not null'+
+    sl +'     , alterada timestamp null '+
+    sl +'     , constraint mensagem_pk primary key (id) '+
+    sl +'     , constraint mensagem_conversa_fk foreign key (conversa_id) references conversa(id) '+
+    sl +'     , constraint mensagem_usuario_fk foreign key (usuario_id) references usuario(id) '+
+    sl +'     ); '+
+    sl +
+    sl +'create '+
+    sl +' table mensagem_conteudo  '+
+    sl +'     ( id serial4 not null '+
+    sl +'     , mensagem_id int4 not null '+
+    sl +'     , ordem int4 not null '+
+    sl +'     , tipo int4 not null '+
+    sl +'     , conteudo bytea null '+
+    sl +'     , constraint mensagem_conteudo_pk primary key (id) '+
+    sl +'     , constraint mensagem_conteudo_mensagem_fk foreign key (mensagem_id) references mensagem(id) '+
+    sl +'     ); '+
+    sl +
+    sl +'create '+
+    sl +' table mensagem_status '+
+    sl +'     ( conversa_id int4 not null '+
+    sl +'     , usuario_id int4 not null '+
+    sl +'     , mensagem_id int4 not null '+
+    sl +'     , recebida timestamp null '+
+    sl +'     , visualizada timestamp null '+
+    sl +'     , reproduzida timestamp null '+
+    sl +'     , constraint mensagem_status_conversa_fk foreign key (conversa_id) references conversa(id) '+
+    sl +'     , constraint mensagem_status_mensagem_fk foreign key (mensagem_id) references mensagem(id) '+
+    sl +'     , constraint mensagem_status_usuario_fk foreign key (usuario_id) references usuario(id) '+
+    sl +'     ); '+
+    sl +
+    sl +'create '+
+    sl +' table anexo '+
+    sl +'     ( id serial4 not null '+
+    sl +'     , identificador varchar(64) not null '+
+    sl +'     , tipo int4 not null '+
+    sl +'     , tamanho int4 not null '+
+    sl +'     , constraint anexo_pk primary key (id) '+
+    sl +'     ); ',
 
+    sl +'ALTER TABLE public.anexo ADD nome varchar(255) NULL; '+
+    sl +'ALTER TABLE public.anexo ADD extensao varchar(10) NULL; '
+  );
 
 procedure Migracoes(iVersao: Integer);
 var
@@ -121,6 +116,7 @@ var
   Qry: TFDQuery;
   iVersaoAtual: Integer;
   sSQL: String;
+  I: Integer;
 begin
   Pool := TPool.Instance;
 
@@ -164,11 +160,18 @@ begin
   end;
 
   sSQL := EmptyStr;
-  if iVersaoAtual < 0 then
-    sSQL := sSQL + v0;
+  for I := Succ(iVersaoAtual) to High(Versoes) do
+    sSQL := sSQL + Versoes[I];
 
   if not sSQL.IsEmpty then
+  begin
+    sSQL := sSQL +
+    sl +'update parametros '+
+    sl +'   set valor = '+ High(Versoes).ToString.QuotedString +
+    sl +' where nome  = ''versao'' ';
+
     TPool.Instance.Connection.ExecSQL(sSQL);
+  end;
 end;
 
 end.
